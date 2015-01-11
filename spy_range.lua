d6a59e5561222f20743ef6b09781a6a61e0335d5
Alexander Lipatov <alexlpt@yandex.ru>
2015-01-11 20:43:37 +0300

    added gremlin too

--- spy_range.lua
+++ spy_range.lua
@@ -1,12 +1,12 @@
 function widget:GetInfo()
     return {
-        name      = "Spy emp and decloack range v2",
-        desc      = "Cloacks spy by default and draws a circle that displays spy decloack range (orange) and spy emp range (blue)",
+        name      = "Spy emp and decloack range v3",
+        desc      = "Cloacks spy by default and draws a circle that displays spy(and gremlin) decloack range (orange) and spy emp range (blue)",
         author    = "[teh]decay aka [teh]undertaker",
         date      = "28 dec 2013",
         license   = "The BSD License",
         layer     = 0,
-        version   = 1,
+        version   = 3,
         enabled   = true  -- loaded by default
     }
 end
@@ -15,6 +15,7 @@ end
 
 --Changelog
 -- v2 [teh]decay Don't draw circles when GUI is hidden
+-- v3 [teh]decay Added gremlin decloack range + set them on hold fire and hold pos
 
 local GetUnitPosition     = Spring.GetUnitPosition
 local glColor = gl.Color
@@ -29,7 +30,9 @@ local spGetPlayerInfo		= Spring.GetPlayerInfo
 local spGiveOrderToUnit = Spring.GiveOrderToUnit
 local spIsGUIHidden = Spring.IsGUIHidden
 
+local CMD_MOVE_STATE    = CMD.MOVE_STATE
 local cmdCloack = CMD.CLOAK
+local cmdFireState = CMD.FIRE_STATE
 
 local blastCircleDivs = 100
 local weapNamTab		  = WeaponDefNames
@@ -41,11 +44,14 @@ local aoeTag = "damageAreaOfEffect"
 
 local coreSpy = UnitDefNames["corspy"]
 local armSpy = UnitDefNames["armspy"]
+local armGremlin = UnitDefNames["armst"]
 
 local coreSpyId = coreSpy.id
 local armSpyId = armSpy.id
+local armGremlinId = armGremlin.id
 
 local spies = {}
+local gremlins = {}
 
 local spectatorMode = false
 local notInSpecfullmode = false
@@ -54,8 +60,21 @@ function cloackSpy(unitID)
     spGiveOrderToUnit(unitID, cmdCloack, { 1 }, {})
 end
 
+function processGremlin(unitID)
+    spGiveOrderToUnit(unitID, cmdCloack, { 1 }, {})
+    spGiveOrderToUnit(unitID, CMD_MOVE_STATE, { 0 }, {}) -- 0 == hold pos
+    spGiveOrderToUnit(unitID, cmdFireState, { 0 }, {}) -- hold fire
+end
+
 function isSpy(unitDefID)
     if unitDefID == coreSpyId or armSpyId == unitDefID then
+        return true
+    end
+    return false
+end
+
+function isGremlin(unitDefID)
+    if unitDefID == armGremlinId then
         return true
     end
     return false
@@ -66,11 +85,20 @@ function widget:UnitFinished(unitID, unitDefID, unitTeam)
         spies[unitID] = true
         cloackSpy(unitID)
     end
+
+    if isGremlin(unitDefID) then
+        gremlins[unitID] = true
+        processGremlin(unitID)
+    end
 end
 
 function widget:UnitDestroyed(unitID, unitDefID, unitTeam)
     if spies[unitID] then
         spies[unitID] = nil
+    end
+
+    if gremlins[unitID] then
+        gremlins[unitID] = nil
     end
 end
 
@@ -80,6 +108,10 @@ function widget:UnitEnteredLos(unitID, unitTeam)
         if isSpy(unitDefID) then
             spies[unitID] = true
         end
+
+        if isGremlin(unitDefID) then
+            gremlins[unitID] = true
+        end
     end
 end
 
@@ -88,12 +120,22 @@ function widget:UnitCreated(unitID, unitDefID, teamID, builderID)
         spies[unitID] = true
         cloackSpy(unitID)
     end
+
+    if isGremlin(unitDefID) then
+        gremlins[unitID] = true
+        processGremlin(unitID)
+    end
 end
 
 function widget:UnitTaken(unitID, unitDefID, unitTeam, newTeam)
     if isSpy(unitDefID) then
         spies[unitID] = true
         cloackSpy(unitID)
+    end
+
+    if isGremlin(unitDefID) then
+        gremlins[unitID] = true
+        processGremlin(unitID)
     end
 end
 
@@ -103,12 +145,21 @@ function widget:UnitGiven(unitID, unitDefID, unitTeam, oldTeam)
         spies[unitID] = true
         cloackSpy(unitID)
     end
+
+    if isGremlin(unitDefID) then
+        gremlins[unitID] = true
+        processGremlin(unitID)
+    end
 end
 
 function widget:UnitLeftLos(unitID, unitDefID, unitTeam)
     if not spectatorMode then
         if spies[unitID] then
             spies[unitID] = nil
+        end
+
+        if gremlins[unitID] then
+            gremlins[unitID] = nil
         end
     end
 end
@@ -146,6 +197,21 @@ function widget:DrawWorldPreUnit()
 
         end
     end
+
+    for unitID in pairs(gremlins) do
+        local x,y,z = GetUnitPosition(unitID)
+        local udefId = GetUnitDefID(unitID);
+        if udefId ~= nil then
+            local udef = udefTab[udefId]
+
+            local selfdBlastId = weapNamTab[lower(udef[selfdTag])].id
+            local selfdBlastRadius = weapTab[selfdBlastId][aoeTag]
+
+            glColor(1, .6, .3, .8)
+            glDrawGroundCircle(x, y, z, udef["decloakDistance"], blastCircleDivs)
+        end
+    end
+
     glDepthTest(false)
 end
 
@@ -166,6 +232,9 @@ function detectSpectatorView()
         spectatorMode = true
     end
 
+    spies = {}
+    gremlins = {}
+
     local visibleUnits = spGetAllUnits()
     if visibleUnits ~= nil then
         for _, unitID in ipairs(visibleUnits) do
@@ -173,6 +242,10 @@ function detectSpectatorView()
             if udefId ~= nil then
                 if isSpy(udefId) then
                     spies[unitID] = true
+                end
+
+                if isGremlin(unitDefID) then
+                    gremlins[unitID] = true
                 end
             end
         end
